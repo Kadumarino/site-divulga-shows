53e080a7c4941a08ec1e04fe4d3561211895ae66
Carlos Eduardo Marino Junior <91035175+Kadumarino@users.noreply.github.com>
2026-09-13 23:42:33 -0300

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const SITE_URL = 'https://kadumarino.github.io';
-const BASE_PATH = '/algumritmobr-page';
-
-// https://astro.build/config
 export default defineConfig({
-  site: SITE_URL,
-  base: BASE_PATH,
+  site: 'https://algumritmobr.com.br',
   trailingSlash: 'always',
   integrations: [sitemap()],
 });
